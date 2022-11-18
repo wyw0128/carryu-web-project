d853c09c5a62b1283ff0aba43f9ce222525e73a3
wyw0128 <yiweiwu0128@gmail.com>
2022-11-18 12:30:19 +1100

    feature: add card component and stories
    chore: house keeping

---
+++ src/globalStyles.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+  font-family:Alibaba PuHuiTi
+  }
+  *, *::after, *::before {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  } 
+`;
+
+export default GlobalStyle;
---
+++ src/styles/mixins.js
@@ -0,0 +1,24 @@
+import { css } from "styled-components";
+import { FontWeights } from "./variables";
+
+export const H2Styles = css`
+  //NOTE: font family should defined at global once
+  //GOOGLE: most of the html tags can inherit font family except: (Button, Input)
+  font-family: inherit;
+  font-weight: ${FontWeights.Bold};
+  font-size: 3rem;
+  line-height: 150%;
+`;
+export const H3Styles = css`
+  font-family: inherit;
+  font-size: 36px;
+  font-weight: ${FontWeights.Normal};
+  line-height: 150%;
+`;
+
+export const BodyStyles = css`
+  font-family: inherit;
+  font-size: 20px;
+  font-weight: ${FontWeights.Normal};
+  line-height: 25px;
+`;
