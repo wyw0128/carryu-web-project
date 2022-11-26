7fec900177d023b22e660eb44e1dac23364233d5
wyw0128 <yiweiwu0128@gmail.com>
2022-11-26 20:47:43 +1100

    fix: video component responsive problem

--- src/components/Video/VideoEmbed.js
+++ src/components/Video/VideoEmbed.js
@@ -4,37 +4,45 @@ import { Skeleton } from "@mui/material";
 import PropTypes from "prop-types";
 import { P } from "../Typography/index";
 import { FontWeights } from "../../styles/variables";
-const VideoWrapper = styled.section`
+const VideoContainer = styled.div`
   width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  justify-content: center;
-  align-items: center;
+  position: relative;
+  padding-top: 56.25%;
 `;
-
+const VideoWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+`;
 const TextContainer = styled(P)`
   font-weight: ${FontWeights.Bold};
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: -3rem;
 `;
 const VideoEmbed = ({ embedId, description, isVideoLoading }) => (
-  <VideoWrapper>
+  <VideoContainer>
     {isVideoLoading ? (
       <Skeleton variant="rounded" animation="wave" width="100%" height="100%" />
     ) : (
-      <iframe
-        // TODO: width & height ratio
-        width="1064px"
-        height="523.7px"
-        src={`https://www.ixigua.com/iframe/${embedId}?autoplay=0`}
-        referrerPolicy="unsafe-url"
-        frameBorder="0"
-        allowFullScreen
-        title={description}
-      />
+      <VideoWrapper>
+        <iframe
+          // TODO: width & height ratio
+          width="100%"
+          height="100%"
+          src={`https://www.ixigua.com/iframe/${embedId}?autoplay=0`}
+          referrerPolicy="unsafe-url"
+          frameBorder="0"
+          allowFullScreen
+          title={description}
+        />
+      </VideoWrapper>
     )}
     <TextContainer>{description}</TextContainer>
-  </VideoWrapper>
+  </VideoContainer>
 );
 
 VideoEmbed.propTypes = {
